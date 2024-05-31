9fecbbe3fd8266647f73170cc5f1e49dd89c4853
okao <hamzathanees@gmail.com>
2024-05-31 22:27:26 +0500

    Add Builders - Add Media Bucket R2 - Cloudflare

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,66 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+// export default [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+export default ({ env }) => [
+  {
+    name: "strapi::logger",
+  },
+  {
+    name: "strapi::errors",
+  },
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, ""),
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, ""),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+  },
+  {
+    name: "strapi::poweredBy",
+  },
+  {
+    name: "strapi::query",
+  },
+  {
+    name: "strapi::body",
+  },
+  {
+    name: "strapi::session",
+  },
+  {
+    name: "strapi::favicon",
+  },
+  {
+    name: "strapi::public",
+  },
 ];
