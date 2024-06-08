c23c34b8ea41e7fa303d9c68bb3dfe77f1bd726e
okao <hamzathanees@gmail.com>
2024-06-08 20:18:33 +0500

    Adding Latin Title and logo Url

---
+++ src/api/logo-url/controllers/logo-url.ts
@@ -0,0 +1,7 @@
+/**
+ * logo-url controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::logo-url.logo-url');
---
+++ src/api/logo-url/services/logo-url.ts
@@ -0,0 +1,7 @@
+/**
+ * logo-url service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::logo-url.logo-url');
